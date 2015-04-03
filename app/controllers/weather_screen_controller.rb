84d3ae0085014f3aaaca54cd965292a021e2cab0
Jon Lim <jonlimtoronto@gmail.com>
2015-04-03 19:59:05 -0400

    Cleaning up a bit and refactoring the weather summary.

--- app/controllers/weather_screen_controller.rb
+++ app/controllers/weather_screen_controller.rb
@@ -28,32 +28,18 @@ class WeatherScreenController < ApplicationController
       end
       
       @client_icon = get_weather_icon(@response)
+      @client_summary = get_weather_summary(@response)
       @current_date = get_date(@response)
       @current_time = get_time(@response)
     end
     
   end
   
-  def json_test
-    if Rails.env.development?
-      client_location = Geocoder.coordinates("33 Bay St, Toronto, ON, Canada")
-      @client_loc = request.location
-      @response = get_weather_in_location(client_location[0], client_location[1])
-      @current_time = DateTime.strptime(@response['currently']['time'].to_s, '%s').strftime('%B %e, %Y')
-    else
-      @client_loc = request.location
-      @response = get_weather_in_location(@client_loc.latitude, @client_loc.longitude)
-      
-      if @client_loc.country == 'United States'
-        @client_temp = get_temperature(@response).round(0).to_s + '°F'
-      else
-        @client_temp = in_celcius(get_temperature(@response)).round(0).to_s + '°C'
-      end
-      
-      @client_icon = get_weather_icon(@response)
-      @current_date = get_date(@response)
-      @current_time = get_time(@response)
-    end
+  def get_weather_in_location(latitude, longitude)
+    call = 'forecast'
+    api_key = ENV['WEATHER_SCREEN_DARK_SKY_API']
+    url = HTTParty.get("https://api.forecast.io/#{call}/#{api_key}/#{latitude},#{longitude}", :headers => { 'ContentType' => 'application/json' })
+    response = JSON.parse(url.body)
   end
   
   def get_date(response)
@@ -62,13 +48,6 @@ class WeatherScreenController < ApplicationController
   
   def get_time(response)
     Time.now.in_time_zone(@response['timezone']).strftime('%r')
-  end
-  
-  def get_weather_in_location(latitude, longitude)
-    call = 'forecast'
-    api_key = ENV['WEATHER_SCREEN_DARK_SKY_API']
-    url = HTTParty.get("https://api.forecast.io/#{call}/#{api_key}/#{latitude},#{longitude}", :headers => { 'ContentType' => 'application/json' })
-    response = JSON.parse(url.body)
   end
   
   def get_temperature(response)
@@ -100,6 +79,10 @@ class WeatherScreenController < ApplicationController
     else
       'wi-tornado'
     end
+  end
+  
+  def get_weather_summary(response)
+    @response['currently']['summary']
   end
   
   def in_celcius(temperature_in_f)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   
   root 'weather_screen#index'
   
-  get 'json_test' => 'weather_screen#json_test'
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
